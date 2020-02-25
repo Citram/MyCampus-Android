35ce58da9782b66789fe5bade11b53f5cc7c1df6
konzert <konzert88@hotmail.com>
2020-02-24 19:49:31 -0500

    Designed main page according to Figma, implemented intent to login page

--- app/src/main/java/io/github/citram/mycampus/MainActivity.kt
+++ app/src/main/java/io/github/citram/mycampus/MainActivity.kt
@@ -1,12 +1,19 @@
 package io.github.citram.mycampus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener{
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
     }
 }
